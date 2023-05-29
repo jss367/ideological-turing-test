a4339f69bab99f33c50b7d5b5adf531aae6cbb88
Julius Simonelli <julius.simonelli@gmail.com>
2023-05-29 16:06:05 -0700

    Update script.js

--- script.js
+++ script.js
@@ -4,11 +4,23 @@ fetch('topics.json')
     .then(response => response.json())
     .then(data => topics = data);
 
-document.getElementById('randomizeBtn').addEventListener('click', function() {
-    let topicData = topics[Math.floor(Math.random() * topics.length)];
-    let position = topicData.positions[Math.floor(Math.random() * topicData.positions.length)];
+let currentTopic = null;
 
-    document.getElementById('topic').textContent = `Your topic is: ${topicData.topic}`;
+document.getElementById('randomizeTopicBtn').addEventListener('click', function() {
+    currentTopic = topics[Math.floor(Math.random() * topics.length)];
+    document.getElementById('topic').textContent = `Your topic is: ${currentTopic.topic}`;
+
+    // Show the result div
+    document.getElementById('result').classList.add('show');
+});
+
+document.getElementById('randomizePositionBtn').addEventListener('click', function() {
+    if (!currentTopic) {
+        alert('You must randomize the topic first.');
+        return;
+    }
+
+    let position = currentTopic.positions[Math.floor(Math.random() * currentTopic.positions.length)];
     document.getElementById('position').textContent = `You need to argue: ${position}`;
 
     // Show the result div
